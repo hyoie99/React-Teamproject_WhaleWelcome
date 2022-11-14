4f5b3e928037c103848e7707375bb3cc2ebde586
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-14 16:46:20 +0900

    [DESIGN] Packed.js - scss 파일 추가

--- src/routes/Packed.js
+++ src/routes/Packed.js
@@ -1,16 +1,44 @@
 import { Link } from "react-router-dom";
+import "../sass/background.scss";
+import "../sass/packed.scss";
 
 function Packed() {
   return (
-    <div>
-      <div>
-        <h1>여러분의 소중한 기록이 안전하게 포장되어 배송됐어요!</h1>
-        <div>
-          <Link to="/choose_another">이전</Link>
-          <Link to="/select_color">다음</Link>
+    <div className="packed-background">
+      <div className="flex-div">
+        <img
+          alt="포장이 완료된 택배상자 이미지"
+          src={require("../img/box_after.png")}
+          className="box-img"
+        ></img>
+        <h1 className="title">
+          여러분의 소중한 기록이 <br />
+          안전하게 포장되어 배송됐어요!
+        </h1>
+        <div className="select-btns">
+          <div className="btn">
+            <img
+              alt="왼쪽 화살표 이미지"
+              src={require("../img/arrow_left.png")}
+              className="arrow-img"
+            ></img>
+            <Link to="/choose_another" className="btn-link">
+              이전
+            </Link>
+          </div>
+          <div className="btn">
+            <Link to="/select_color" className="btn-link">
+              다음
+            </Link>
+            <img
+              alt="오른쪽 화살표 이미지"
+              src={require("../img/arrow_right.png")}
+              className="arrow-img"
+            ></img>
+          </div>
         </div>
       </div>
-      <button>건너뛰기</button>
+      <button className="btn-skip">건너뛰기</button>
     </div>
   );
 }
