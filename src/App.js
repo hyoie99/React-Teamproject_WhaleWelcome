b8a3e8e638d9ad5fb524769c0c2a27eb9b101f52
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-17 02:15:50 +0900

    [CHORE] basename 수정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import End from "./routes/End";
 function App() {
   return (
     <BrowserRouter>
-      <Routes basename={process.env.PUBLIC_URL}>
+      <Routes basename="/React-Teamproject_WhaleWelcome">
         <Route path="/" element={<Home />}></Route>
         <Route path="/choose_another" element={<AnotherBrowser />}></Route>
         <Route path="/packed" element={<Packed />}></Route>
