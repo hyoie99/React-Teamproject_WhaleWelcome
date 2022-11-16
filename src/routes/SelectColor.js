3f7f184c9af9e633e9efd6f8e12f3eaa3bfa50e0
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-16 11:18:50 +0900

    [DESIGN] SelectColor.js - colorchip css 변경

--- src/routes/SelectColor.js
+++ src/routes/SelectColor.js
@@ -5,7 +5,7 @@ import style from "../css/color.module.css";
 function SelectColor() {
   return (
     <div className={common.background}>
-      <div className={common.flex_div}>
+      <div className={style.flex_div}>
         <h1 className={common.title}>
           새로 입주하실 브라우저의 <br /> 테마 컬러를 골라볼까요?
         </h1>
@@ -24,21 +24,35 @@ function SelectColor() {
             <div id={style.color8} className={style.color_chip}></div>
             <div id={style.color9} className={style.color_chip}></div>
             <div id={style.color10} className={style.color_chip}></div>
-            <div id={style.color11} className={style.color_chip}></div>
-            <div id={style.color12} className={style.color_chip}></div>
-            <div id={style.color13} className={style.color_chip}></div>
-            <div id={style.color14} className={style.color_chip}></div>
-            <div id={style.color15} className={style.color_chip}></div>
-            <div id={style.color16} className={style.color_chip}></div>
+            <div id={style.color11} className={style.color_chip}>
+              <div id={style.color11_2}></div>
+            </div>
+            <div id={style.color12} className={style.color_chip}>
+              <div id={style.color12_2}></div>
+            </div>
+            <div id={style.color13} className={style.color_chip}>
+              <div id={style.color13_2}></div>
+            </div>
+            <div id={style.color14} className={style.color_chip}>
+              <div id={style.color14_2}></div>
+            </div>
+            <div id={style.color15} className={style.color_chip}>
+              <div id={style.color15_2}></div>
+            </div>
+            <div id={style.color16} className={style.color_chip}>
+              <div id={style.color16_2}></div>
+            </div>
           </div>
           <div className={style.theme_img}>
             <img
               alt="색상이 적용되지 않은 브라우저 탭 이미지"
               src={require("../img/theme_color0.png")}
+              className={style.change_img}
             ></img>
             <img
               alt="네이버 메인화면 이미지"
               src={require("../img/theme_fix.png")}
+              className={style.fix_img}
             ></img>
           </div>
         </div>
@@ -62,7 +76,7 @@ function SelectColor() {
           </Link>
         </div>
       </div>
-      <button className={common.skip_btn}>건너뛰기</button>
+      <button className={style.skip_btn}>건너뛰기</button>
     </div>
   );
 }
