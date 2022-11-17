807698f90b75f5fea0a4de4b87b3333304e1cc85
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-17 11:26:13 +0900

    [ADD] Packaging.js - 포장 비디오 추가

--- src/components/Packaging.js
+++ src/components/Packaging.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import ReactPlayer from "react-player";
+import packagingVideo from "../video/packaging.mp4";
 import common from "../css/common.module.css";
 import style from "../css/packaging.module.css";
 
@@ -8,7 +10,7 @@ function Packaging() {
   const timeout = () => {
     setTimeout(() => {
       navigate("/packed");
-    }, 1500);
+    }, 8000);
   };
 
   useEffect(() => {
@@ -18,9 +20,16 @@ function Packaging() {
 
   return (
     <div className={common.background}>
-      <div className={style.flex_div}>
-        <div className={style.animation}></div>
-        <h1 className={common.title}>북마크 내역을 포장하는 중...</h1>
+      <div className={style.div}>
+        <ReactPlayer
+          url={packagingVideo}
+          playing
+          muted
+          width="130%"
+          height="120%"
+          className={style.video}
+        />
+        <h1 className={style.title}>북마크 내역을 포장하는 중...</h1>
       </div>
     </div>
   );
