32a946fda45cc0a9a5ade8625f700d0731b7000d
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-17 11:36:16 +0900

    [ADD] End.js - 완료 비디오 추가

--- src/routes/End.js
+++ src/routes/End.js
@@ -1,3 +1,5 @@
+import ReactPlayer from "react-player";
+import welcomeVideo from "../video/welcome.mp4";
 import common from "../css/common.module.css";
 import style from "../css/end.module.css";
 
@@ -8,7 +10,16 @@ function End() {
         성공적으로 이사를 마쳤어요! <br />
         사용자님에게 딱 맞는 브라우저에서의 <br /> 산뜻한 새 시작을 기원할게요
       </h1>
-      <img className={style.image} src={require("../img/welcome.png")}></img>
+      <button className={style.single_btn}>완료</button>
+      <ReactPlayer
+        url={welcomeVideo}
+        playing
+        muted
+        loop
+        width="100vw"
+        height="100vh"
+        className={style.video}
+      />
     </div>
   );
 }
