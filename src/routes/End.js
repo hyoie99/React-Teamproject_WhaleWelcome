2f86fc8eee043c159f818510128d94093e29b1e2
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-18 03:32:04 +0900

    [CHORE] 수정사항 반영

--- src/routes/End.js
+++ src/routes/End.js
@@ -8,7 +8,7 @@ function End() {
     <div className={common.background}>
       <h1 className={style.title}>
         성공적으로 이사를 마쳤어요! <br />
-        사용자님에게 딱 맞는 브라우저에서의 <br /> 산뜻한 새 시작을 기원할게요
+        나에게 딱 맞는 브라우저에서의 <br /> 산뜻한 새 시작을 기원할게요
       </h1>
       <button className={style.single_btn}>완료</button>
       <ReactPlayer
