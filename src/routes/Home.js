818b780be99ac3c0c6df20293e2460ac45a81a94
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-18 01:11:06 +0900

    [CHORE] Home.js - 타이틀 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,9 +6,10 @@ function Home() {
     <div className={common.background}>
       <div className={common.flex_div}>
         <h1 className={common.title}>
-          더 편리하고 빠른 웨일 브라우저로의 입주를 결정하셨군요! <br />
-          그럼 이사를 시작해볼까요?
+          더 편리하고 빠른 <br />
+          웨일 브라우저로의 입주를 결정하셨군요!
         </h1>
+        <p className={common.description}>그럼 이사를 시작해볼까요?</p>
         <img
           alt="웨일 브라우저로의 이사를 위한 트럭입니다."
           src={require("../img/truck.png")}
