09e0a573188f390d8fbc00cccd75a3fda981b2f5
hyein <80624327+hyoie99@users.noreply.github.com>
2022-11-18 01:28:26 +0900

    [CHORE] skip-btn link 수정

--- src/components/Select.js
+++ src/components/Select.js
@@ -44,15 +44,7 @@ function Select({ setLoading }) {
           </a>
         </div>
       </div>
-      {/* <button
-        onClick="location.href='whale://newtab/'"
-        className={common.skip_btn}
-      >
-        건너뛰기
-      </button> */}
-      <a href="whale://newtab/" className={common.skip_btn}>
-        건너뛰기
-      </a>
+      <button className={common.skip_btn}>건너뛰기</button>
     </div>
   );
 }
